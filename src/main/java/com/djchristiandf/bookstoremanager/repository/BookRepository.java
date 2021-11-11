b315f0776844984a4387e063b435c53264b4d5b6
Christian S Barbosa <djchristiandf@DJCHRISTIANDF.localdomain>
2021-11-10 22:45:48 -0300

    add repository jparepository for book

---
+++ src/main/java/com/djchristiandf/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.djchristiandf.bookstoremanager.repository;
+
+import com.djchristiandf.bookstoremanager.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
